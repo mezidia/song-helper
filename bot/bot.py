643a4cdfbf079166eec36572e534ec3430b145ab
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-13 09:41:47 +0200

    Get token form env variables

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,14 @@
 import logging
+import os
 
 from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token = '?')
+Token = os.getenv('TOKEN')
+bot = Bot(token = Token)
 dp = Dispatcher(bot)
 
 @dp.message_handler()
