2d7244211aba7e9ee20dc7e8d33539adc14daa5a
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-02-25 17:48:32 +0200

    - Tests reworked - #4

--- bot/audiodownloader.py
+++ bot/audiodownloader.py
@@ -3,8 +3,11 @@ import os
 
 
 def audio_downloader(url): 
-    type_of_audio_file = 3
+    type_of_audio_file = 1
     video = pafy.new(url)
     audiostreams = video.audiostreams
+    print(audiostreams)
     audiostreams[type_of_audio_file].download(filepath = './bot/')
     return f'bot/{video.title}.{audiostreams[type_of_audio_file].extension}'
+
+audio_downloader('https://www.youtube.com/watch?v=YAZGKbAp36E')
