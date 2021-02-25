7ff62b7521fa81a328eaa7f33e1c4b63bf88c6fb
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-02-25 10:21:35 +0200

    - Start commands created -

--- bot/bot.py
+++ bot/bot.py
@@ -12,9 +12,12 @@ bot = Bot(token=Token)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm SongHelperBot!\nTo get started, send your mood with a text message.")
 
 
 if __name__ == '__main__':
