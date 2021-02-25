5d358d46f688c652c32763aac726947eb5d46bee
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-02-25 17:59:10 +0200

    - Tests reworked - #4

--- bot/audiodownloader.py
+++ bot/audiodownloader.py
@@ -6,7 +6,7 @@ def audio_downloader(url):
     video = pafy.new(url)
     audiostreams = video.audiostreams
     for i in range(len(audiostreams)):
-        if 'm4a' in audiostreams[i]:
+        if 'm4a' in str(audiostreams[i]):
             audiostreams[i].download(filepath = './bot/')
             return f'bot/{video.title}.{audiostreams[i].extension}'
     print(audiostreams)
