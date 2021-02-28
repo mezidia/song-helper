61e22d4191603c3f15a61bd7707b011d14c9736e
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-02-28 16:07:38 +0200

    Revert "- Connection with server created - #4"
    This reverts commit 026ea891acf36623832b785d785c3582c009c917.

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import pafy
-import requests
 from audiodownloader import audio_downloader
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -21,15 +20,11 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    print(message)
     await message.reply("Hi!\nI'm SongHelperBot!\nTo get started, send your mood with a text message.")
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    # url = ''
-    # msg_to_server = {'msg': message.text}
-    # song = requests.post(url, msg_to_server).text
     path = audio_downloader('https://www.youtube.com/watch?v=jdGe4w4LADM')
     audio = open(path, 'rb')
     message = await bot.send_audio(message['chat']['id'], audio)
