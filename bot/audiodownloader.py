13e1bef9b7991cac8856a6045842b73d4685ad54
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-02-28 16:13:55 +0200

    Update audiodownloader.py

--- bot/audiodownloader.py
+++ bot/audiodownloader.py
@@ -13,6 +13,3 @@ def audio_downloader(url: str) -> str:
         if 'm4a' in str(audiostreams[i]):
             audiostreams[i].download(filepath='./bot/')
             return f'bot/{video.title}.{audiostreams[i].extension}'
-
-
-audio_downloader('https://www.youtube.com/watch?v=YAZGKbAp36E')
