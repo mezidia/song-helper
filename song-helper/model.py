c048730f9e8320f9f2e77a906c787d9c1fae0021
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-04 22:48:55 +0200

    #14 Create files
    Create __init__.py file and model.py with Keras model. Install packages

---
+++ song-helper/model.py
@@ -0,0 +1,15 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+def base_model():
+    # Create the model
+    model = Sequential()
+    # Add 1 layer with 8 nodes,input of 4 dim with relu function
+    model.add(Dense(8, input_dim=10, activation='relu'))
+    # Add 1 layer with output 3 and softmax function
+    model.add(Dense(4, activation='softmax'))
+    # Compile the model using sigmoid loss function and adam optim
+    model.compile(loss='categorical_crossentropy', optimizer='adam',
+                  metrics=['accuracy'])
+    return model
