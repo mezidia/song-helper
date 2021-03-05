69263103a5d7612092d887434bd8647b0b8ed6c5
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-05 10:42:03 +0200

    #16 Update models.py
    Create classes Mood and Song

--- server/mesite/models.py
+++ server/mesite/models.py
@@ -1,6 +1,54 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Mood(models.Model):
+    """
+    Class that represents Mood characteristic of song
+    fields: mood
+    """
+    MOODS = (
+        ('H', 'Happy'),
+        ('E', 'Energetic'),
+        ('C', 'Calm'),
+        ('S', 'Sad'),
+    )
+    mood = models.CharField(max_length=1, choices=MOODS, blank=False)
+
+    def __str__(self):
+        return self.mood
+
+    class Meta:
+        verbose_name = 'Mood'
+        verbose_name_plural = 'Moods'
+
+
+class Song(models.Model):
+    """
+    Class that represents Song object
+    fields: name, artist, id, acousticness, danceability, energy, instrumentalness,
+    liveness, valence, loudness, speechiness, tempo, key, time_signature, mood
+    """
+    name = models.CharField('Name', blank=False, max_length=40)
+    artist = models.CharField('Artists', blank=False, max_length=20)
+    id = models.CharField('Id', unique=True)
+    acousticness = models.FloatField()
+    danceability = models.FloatField()
+    energy = models.FloatField()
+    instrumentalness = models.FloatField()
+    liveness = models.FloatField()
+    valence = models.FloatField()
+    loudness = models.FloatField()
+    speechiness = models.FloatField()
+    tempo = models.FloatField()
+    key = models.IntegerField()
+    time_signature = models.IntegerField()
+    mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'"{self.name}" is a {self.mood} song'
+
+    class Meta:
+        verbose_name = 'Song'
+        verbose_name_plural = 'Songs'
+        ordering = ['-name']
+
