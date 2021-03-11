d7e6a016da53a50cb5a1ed3fe5e0f23f9e950ccc
VsIG <valentyn2202@gmail.com>
2021-03-11 21:41:43 +0200

    ✨: add getting vector for sentence

--- song-helper/MoodFromWords.py
+++ song-helper/MoodFromWords.py
@@ -10,15 +10,20 @@
 import spacy
 
 # Load the spacy model that you have installed
-nlp = spacy.load('en_core_web_trf')
+nlp = spacy.load('en_core_web_md')
 
 # process a sentence using the model
-doc = nlp("Sad")
+doc = nlp("I am very sad")
+tokens = doc
+
+for token in tokens:
+    print(token.text, token.has_vector, token.vector_norm, token.is_oov)
+
+print(doc.vector)
 
 # It's that simple - all of the vectors and words are assigned after this point
-# Get the vector for 'text':
-doc[3].vector
+# Get the vector for 'sad':
+# vector3 = doc[3].vector
 
 # Get the mean vector for the entire sentence (useful for sentence classification etc.)\
-doc.vector
-print("Some")
+# vector = doc.vector
