ca863cfbe538e58bf1af44e9f534ea5260f88389
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-11 12:38:25 +0200

    Update test_utils.py
    Test fields of object

--- song-helper/test_utils.py
+++ song-helper/test_utils.py
@@ -1,12 +1,29 @@
+import spotipy
+
 from unittest import TestCase
 
 from .utils import SpotifyUtils
 
 
 class TestSpotifyUtils(TestCase):
+    def setUp(self) -> None:
+        self.util_obj = SpotifyUtils()
+
     def test_creation(self):
-        util = SpotifyUtils()
-        self.assertIsInstance(util, SpotifyUtils)
+        """Our object must be an instance of SpotifyUtils class"""
+        self.assertIsInstance(self.util_obj, SpotifyUtils)
+
+    def test_default_fields(self):
+        """If we do not set params in constructor, they must be set as default. And test their types"""
+        self.assertIsNotNone(self.util_obj.spt)
+        self.assertIsInstance(self.util_obj.spt, spotipy.Spotify)
+        self.assertIsNotNone(self.util_obj.redirect_uri)
+        self.assertIsInstance(self.util_obj.redirect_uri, str)
+        self.assertIsNotNone(self.util_obj.client_secret)
+        self.assertIsInstance(self.util_obj.client_secret, str)
+        self.assertIsNotNone(self.util_obj.client_id)
+        self.assertIsInstance(self.util_obj.client_id, str)
+
 
     # def test_get_albums_id(self):
     #     self.fail()
