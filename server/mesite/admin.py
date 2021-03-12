a81164a00adc35f9535625dcf5a161c6c1d14b06
fhrr-sht <54956154+fhrr-sht@users.noreply.github.com>
2021-03-12 13:25:58 +0200

    Fix some things

--- server/mesite/admin.py
+++ server/mesite/admin.py
@@ -11,6 +11,6 @@ class MoodAdmin(admin.ModelAdmin):
 
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('name', 'artist', 'song_id', 'mood')
+    list_display = ('name', 'artists', 'song_id', 'mood')
     list_filter = ('mood',)
-    search_fields = ['name', 'artist']
+    search_fields = ['name', 'artists']
