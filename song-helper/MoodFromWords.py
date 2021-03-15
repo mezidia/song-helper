1a5d725d1eeb8d35fe8e13b9255b90180023d439
VsIG <valentyn2202@gmail.com>
2021-03-15 11:49:08 +0200

    ✨: start using spacy and sklearn

--- song-helper/MoodFromWords.py
+++ song-helper/MoodFromWords.py
@@ -1,4 +1,15 @@
+# for creating dataframe
 import pandas as pd
+import spacy as sp
+# ML Packages
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.base import TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+
+# Load the spacy model that you have installed
+nlp = sp.load('en_core_web_md')
 
 filepath = {'emorynlp_train_final.csv'}
 
