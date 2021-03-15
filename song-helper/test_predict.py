7636f32fdab0f7bd9ce79d0816320403eea5c8cc
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-15 14:51:49 +0200

    Update test_predict.py
    Set new song

--- song-helper/test_predict.py
+++ song-helper/test_predict.py
@@ -10,25 +10,25 @@ class TestPredictMood(TestCase):
         self.spt = SpotifyUtils()
         self.assertIsNotNone(self.predict)
         self.assertIsNotNone(self.spt)
-        self.id = '4Km5HrUvYTaSUfiSGPJeQR'
+        self.id = '2H7PHVdQ3mXqEHXcvclTB0'
 
     def test_prepare_data(self):
         """Returned data must be customized"""
         data = self.spt.get_song(self.id)[6:-2]
-        expected = [343150.0, 0.927, 0.061, 0.665, 0.0, 0.123, 0.175, -5.313, 0.244, 127.076]
+        expected = [379266.0, 0.866, 0.137, 0.73, 0.0, 0.0843, 0.625, -8.201, 0.0767, 118.523]
         self.assertIsNotNone(self.predict.prepare_data(data))
         self.assertEqual(self.predict.prepare_data(data), expected)
 
     def test_prepare_artists(self):
         """Returned string must be without list symbols"""
         data = self.spt.get_song(self.id)[2][1]
-        expected = "'Migos', 'Lil Uzi Vert'"
+        expected = "'Prince'"
         self.assertIsNotNone(self.predict.prepare_artists(data))
         self.assertEqual(self.predict.prepare_artists(data), expected)
 
     def test_predict_mood(self):
         """Test the output from function that predicts the mood"""
         output = self.predict.predict_mood(self.id, 'song-helper/data/data_moods.csv')
-        expected = "Bad and Boujee (feat. Lil Uzi Vert) by 'Migos', 'Lil Uzi Vert' is a HAPPY song"
+        expected = "1999 by 'Prince' is a HAPPY song"
         self.assertIsNotNone(output)
         self.assertEqual(output, expected)
