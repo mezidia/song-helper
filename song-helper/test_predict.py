a3ad0ea6c42bdae66671d238495a849f54e8806c
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-15 12:07:57 +0200

    Update test_predict.py
    Test predictiong the mood

--- song-helper/test_predict.py
+++ song-helper/test_predict.py
@@ -27,4 +27,8 @@ class TestPredictMood(TestCase):
         self.assertEqual(self.predict.prepare_artists(data), expected)
 
     def test_predict_mood(self):
-        self.skipTest('Not ready')
+        """Test the output from function that predicts the mood"""
+        output = self.predict.predict_mood(self.id, 'song-helper/data/data_moods.csv')
+        expected = "Bad and Boujee (feat. Lil Uzi Vert) by 'Migos', 'Lil Uzi Vert' is a ENERGETIC song"
+        self.assertIsNotNone(output)
+        self.assertEqual(output, expected)
