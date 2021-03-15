78cf98938540f0d7abd02d6f2ad288d5981f32df
VsIG <valentyn2202@gmail.com>
2021-03-15 11:35:42 +0200

    ✨: add reading from .csv

--- song-helper/MoodFromWords.py
+++ song-helper/MoodFromWords.py
@@ -1,20 +1,6 @@
-import spacy
+import pandas as pd
 
-# Load the spacy model that you have installed
-nlp = spacy.load('en_core_web_md')
+filepath = {'emorynlp_train_final.csv'}
 
-# process a sentence using the model
-doc = nlp("I am very sad")
-tokens = doc
-
-for token in tokens:
-    print(token.text, token.has_vector, token.vector_norm, token.is_oov)
-
-print(doc.vector)
-
-# It's that simple - all of the vectors and words are assigned after this point
-# Get the vector for 'sad':
-# vector3 = doc[3].vector
-
-# Get the mean vector for the entire sentence (useful for sentence classification etc.)\
-# vector = doc.vector
+df=pd.read_csv('docs\MELD-dataset\data\emorynlp\emorynlp_train_final.csv')
+print(df)
