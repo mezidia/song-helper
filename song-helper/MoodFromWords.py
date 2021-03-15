7d92442795905d281924f49f13d59749bdabaa38
VsIG <valentyn2202@gmail.com>
2021-03-15 11:39:55 +0200

    ✨: add reading specific columns from .csv

--- song-helper/MoodFromWords.py
+++ song-helper/MoodFromWords.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 filepath = {'emorynlp_train_final.csv'}
 
-df=pd.read_csv('docs\MELD-dataset\data\emorynlp\emorynlp_train_final.csv')
+df=pd.read_csv('docs\MELD-dataset\data\emorynlp\emorynlp_train_final.csv',usecols=['Utterance', 'Emotion'])
 print(df)
