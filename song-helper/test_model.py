7bc796c969db5e7e1732ae9315c860c9d7e2fdee
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-15 15:39:13 +0200

    Update test_model.py

--- song-helper/test_model.py
+++ song-helper/test_model.py
@@ -11,7 +11,6 @@ class TestModel(TestCase):
         expected_units = 8
         expected_units_1 = 4
         expected_layer_name = 'dense'
-        expected_layer_name_1 = 'dense_1'
         self.assertTrue(model.name.startswith(expected_name))
         self.assertEqual(len(model.layers), 2)
         self.assertEqual(model.layers[0].units, expected_units)
