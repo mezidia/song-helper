d3b98854ea883b835bbc39176c314ab06962e236
VsIG <valentyn2202@gmail.com>
2021-03-15 12:09:46 +0200

    ✨: add lemmatization

--- song-helper/MoodFromWords.py
+++ song-helper/MoodFromWords.py
@@ -1,6 +1,7 @@
 # for creating dataframe
 import pandas as pd
 import spacy as sp
+from spacy.lang.en.stop_words import STOP_WORDS
 # ML Packages
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.metrics import accuracy_score
@@ -14,4 +15,27 @@ nlp = sp.load('en_core_web_md')
 filepath = {'emorynlp_train_final.csv'}
 
 df=pd.read_csv('docs\MELD-dataset\data\emorynlp\emorynlp_train_final.csv',usecols=['Utterance', 'Emotion'])
-print(df)
+#print(df)
+
+# Checking for missing values
+#print(df.isnull().sum())
+
+# Build a list of stopwords to use to filter
+stopWords = list(STOP_WORDS)
+
+# There You should read input
+docx = nlp("This is how John Walker was walking. He was also running beside the lawn.")
+
+print("\nLemmatizing of tokens:\n")
+
+# Lemmatizing of tokens
+for word in docx:
+    print(word.text,"Lemma =>",word.lemma_)
+
+print("\nLemma that are not pronouns:\n")
+
+# Lemma that are not pronouns
+for word in docx:
+    if word.lemma_ != "-PRON-":
+        print(word.lemma_.lower().strip())
+
