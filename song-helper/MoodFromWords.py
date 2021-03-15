51222c785c5f29fd1ceb8984b3b75333f6150345
VsIG <valentyn2202@gmail.com>
2021-03-15 17:05:30 +0200

    ✨: replace some values in dataset

--- song-helper/MoodFromWords.py
+++ song-helper/MoodFromWords.py
@@ -18,11 +18,9 @@ import string
 # Load the spacy model that you have installed
 nlp = sp.load('en_core_web_md')
 
-filepath = {'emorynlp_train_final.csv'}
-
-df=pd.read_csv('docs\MELD-dataset\data\emorynlp\emorynlp_train_final.csv',usecols=['Utterance', 'Emotion'])
+df=pd.read_csv('docs\OurData.csv',usecols=['Utterance', 'Emotion'])
 #print(df)
-df.to_csv('docs\OurData.csv', index = False)
+
 # Checking for missing values
 #print(df.isnull().sum())
 
