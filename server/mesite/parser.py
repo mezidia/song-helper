66c6d0bf6c78809d4b90205d833d06480fe96c4b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-16 17:23:07 +0200

    Update parser.py
    First get request

--- server/mesite/parser.py
+++ server/mesite/parser.py
@@ -6,3 +6,16 @@ URL = 'https://www.youtube.com/results?search_query=python'
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Mobile Safari/537.36',
     'accept': '*/*'}
+
+
+def get_html(url: str, params=None):
+    response = requests.get(url, headers=HEADERS, params=params)
+    return response
+
+
+def parse():
+    html = get_html(URL)
+    print(html)
+
+
+parse()
