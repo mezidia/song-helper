3241177f00c79dafcdba309e70bc1f7efcda6b70
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-17 09:29:08 +0200

    #26 #27 Update searcher.py
    Rename file and add docstring

--- server/mesite/searcher.py
+++ server/mesite/searcher.py
@@ -3,6 +3,12 @@ from youtubesearchpython import VideosSearch
 
 
 def search_youtube(value: str, parameters=None) -> dict:
+    """
+    Function to search only first video on YouTube
+    :param value: what we need to search
+    :param parameters: some parameters, such as limit, language, region
+    :return: dictionary with main information
+    """
     search = VideosSearch(value, **parameters)
     result = search.result()['result'][0]
     return {
