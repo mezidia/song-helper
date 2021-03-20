7436a6e5d13b347c636dc291c1b9b2a4571f2598
VsIG <valentyn2202@gmail.com>
2021-03-20 21:48:12 +0200

    ✨: add code for tokens, filtering and lemmanization

--- song-helper/MoodFromWords.py
+++ song-helper/MoodFromWords.py
@@ -1,27 +1,38 @@
 import spacy as sp
 
-class MoodOfWords:
-    """
-    This class receives sentences from the user and analyzes them
-    for belonging to one of the four emotions:
-    Happy, Sad, Calm (Neutral) and Energetic (Angry)
-    Returns:
-        [Mood]: [emotional represantion of sentences]
-    """
+# class MoodOfWords:
+#     """
+#     This class receives sentences from the user and analyzes them
+#     for belonging to one of the four emotions:
+#     Happy, Sad, Calm (Neutral) and Energetic (Angry)
+#     Returns:
+#         [Mood]: [emotional represantion of sentences]
+#     """
 
-    # Load the spacy model that you have installed
-    nlp = sp.load('en_core_web_sm')
+# Load the spacy model that you have installed
+nlp = sp.load('en_core_web_sm')
 
-    # User text
-    # There You should read input
-    user_text = "This is how John Walker was walking. He was also running beside the lawn."
+# User text
+# There You should read input
+user_text = "This is how John Walker was walking. He was also running beside the lawn."
 
-    # Load text as spacy component
-    doc = nlp(user_text)
+# Load text as spacy component
+doc = nlp(user_text)
 
-def create_tokens(doc):
-    token_list = [token for token in doc]
-    print(type(token_list))
+# Create tokens
+token_list = [token for token in doc]
+print(token_list)
+
+# Create and filter tokens
+filtered_tokens = [token for token in doc if not token.is_stop]
+print(filtered_tokens)
+
+# Lemmanization
+lemmas = [
+    f"Token: {token}, lemma: {token.lemma_}"
+    for token in filtered_tokens
+]
+print(lemmas)
 
 #    def lemmatize():
 
