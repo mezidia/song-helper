62b0cbf276a983a6b38d9238097c587075e12a1c
VsIG <valentyn2202@gmail.com>
2021-03-23 02:04:19 +0200

    ✨: add new test class

--- song-helper/test_word_preprocessor.py
+++ song-helper/test_word_preprocessor.py
@@ -6,7 +6,42 @@ class TestWordPreprocessor(unittest.TestCase):
     def setUp(self):
         self.preprocessor = WordPreprocessor()
 
+    # region Equal Tests
 
+    def test_clean_text_equal_Passing_text(self):
+        result = self.preprocessor.clean_text("Passing test")
+        expected = "passing test"
+        self.assertEqual(result, expected)
+
+    def test_clean_text_not_equal_Passing_text(self):
+        result = self.preprocessor.clean_text("Passing test")
+        expected = "not passing test"
+        self.assertNotEqual(result, expected)
+
+    def test_clean_text_equal_new_text(self):
+        result = self.preprocessor.clean_text("new text")
+        expected = "new text"
+        self.assertEqual(result, expected)
+
+    def test_clean_text_not_equal_new_text(self):
+        result = self.preprocessor.clean_text("new text")
+        expected = "not new text"
+        self.assertNotEqual(result, expected)
+
+    # endregion Equal Tests
+
+    # region Instance Tests
+
+    def test_instance_PredictMood_predictor(self):
+        self.assertIsInstance(self.preprocessor, WordPreprocessor)
+
+    def test_not_instance_PredictMood_predictor(self):
+        self.assertNotIsInstance(self.preprocessor, TestWordPreprocessor)
+
+    # endregion Instance Tests
+
+    def tearDown(self):
+        del self.preprocessor
 
 if __name__ == '__main__':
     unittest.main()
