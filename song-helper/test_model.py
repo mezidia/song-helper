570da76f2ca3e007690e2f85a71d3b82cd539c7b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-23 09:26:47 +0200

    #14 Update test_model.py
    Create setUp and tearDonw functions

--- song-helper/test_model.py
+++ song-helper/test_model.py
@@ -4,16 +4,21 @@ from .model import make_model
 
 
 class TestModel(TestCase):
+    def setUp(self) -> None:
+        self.model = make_model()
+
     def test_make_model(self):
         """Test creation the keras model"""
-        model = make_model()
         expected_name = 'sequential'
         expected_units = 8
         expected_units_1 = 4
         expected_layer_name = 'dense'
-        self.assertTrue(model.name.startswith(expected_name))
-        self.assertEqual(len(model.layers), 2)
-        self.assertEqual(model.layers[0].units, expected_units)
-        self.assertEqual(model.layers[1].units, expected_units_1)
-        self.assertTrue(model.layers[0].name.startswith(expected_layer_name))
-        self.assertTrue(model.layers[1].name.startswith(expected_layer_name))
+        self.assertTrue(self.model.name.startswith(expected_name))
+        self.assertEqual(len(self.model.layers), 2)
+        self.assertEqual(self.model.layers[0].units, expected_units)
+        self.assertEqual(self.model.layers[1].units, expected_units_1)
+        self.assertTrue(self.model.layers[0].name.startswith(expected_layer_name))
+        self.assertTrue(self.model.layers[1].name.startswith(expected_layer_name))
+
+    def tearDown(self) -> None:
+        del self.model
