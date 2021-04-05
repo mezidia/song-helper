41b021e1b721e988442f0d38d325357b07248c10
VsIG <valentyn2202@gmail.com>
2021-04-05 21:51:29 +0300

    ✨: add working text getter

--- server/mesite/views.py
+++ server/mesite/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
 from .forms import MainForm
 
 
 def index(request):
+    result = ''
+    if request.method == 'POST':
+        form = MainForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            result = text.capitalize()
     context = {
+        'result': result,
         'form': MainForm(),
     }
     return render(request, 'mesite/index.html', context)
