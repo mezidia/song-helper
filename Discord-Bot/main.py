2d2a931f0f75c4b051b930c3a58b212e83802041
VsIG <valentyn2202@gmail.com>
2021-04-07 14:09:05 +0300

    Update main.py

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -6,15 +6,15 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+	print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
 	if message.author == client.user:
 		return
-	
+
 	if message.content.startswith('sh!hello'):
 		await message.channel.send('Hello!')
-	
+
 keep_alive()
 client.run(os.getenv('TOKEN'))
