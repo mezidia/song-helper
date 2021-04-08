346916825122047c48c5c0d28b54f3682ea1decc
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-04-08 19:59:11 +0000

    ✨: Add better imports

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -1,12 +1,18 @@
 import discord
+from discord.ext import commands
 import os
 from keep_alive import keep_alive
 
-client = discord.Client()
+client = commands.Bot(command_prefix = 'sh!')
 
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
+
+@client.command(pass_context=True)
+async def join(ctx):
+	channel = ctx.message.author.voice.voice_channel
+	await client.join_voice_channel(channel)
 
 @client.event
 async def on_message(message):
@@ -14,7 +20,7 @@ async def on_message(message):
 		return
 
 	if message.content.startswith('sh!hello'):
-		await message.channel.send('Hello!')
+		await message.channel.send('Hi!')
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
