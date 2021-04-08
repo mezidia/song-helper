9ecba890258e9fac75e653042b02b9f3784f24c5
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-04-08 21:07:09 +0000

    ✨: add working join and leave

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 from keep_alive import keep_alive
 
-client = commands.Bot(command_prefix = 'sh!')
+client = commands.Bot(command_prefix='sh!')
 
 @client.event
 async def on_ready():
@@ -12,20 +12,11 @@ async def on_ready():
 @client.command()
 async def join(ctx):
 	channel = ctx.author.voice.channel
-	print(channel)
 	await channel.connect()
 
 @client.command()
 async def leave(ctx):
 	await ctx.voice_client.disconnect()
-
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-
-	if message.content.startswith('sh!hello'):
-		await message.channel.send('Hi!')
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
