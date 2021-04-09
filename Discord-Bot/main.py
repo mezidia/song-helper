12b5db6886399c68299849a2193a26ba1ae941fd
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-04-09 09:46:54 +0000

    ✨: try to add play feature

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -2,8 +2,11 @@ import discord
 from discord.ext import commands
 import os
 from keep_alive import keep_alive
+import youtube_dl
 
 client = commands.Bot(command_prefix='sh!')
+
+players = {}
 
 @client.event
 async def on_ready():
@@ -17,6 +20,13 @@ async def join(ctx):
 @client.command()
 async def leave(ctx):
 	await ctx.voice_client.disconnect()
+
+@client.command()
+async def play(ctx, url: str):
+	server = ctx.message.server
+	player = await ctx.voice_client.create_ytdl_player(url)
+	players[server.id] = player
+	player.start
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
