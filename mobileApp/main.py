579989edcf19a82ff04a2a9a45cd2ad91b4ac7c3
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-11 20:55:57 +0300

    Update main.py
    Work with layouts

--- mobileApp/main.py
+++ mobileApp/main.py
@@ -1,15 +1,30 @@
+import kivy
+import random
+
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+red = [1, 0, 0, 1]
+green = [0, 1, 0, 1]
+blue = [0, 0, 1, 1]
+purple = [1, 0, 1, 1]
 
 
-class MainApp(App):
+class HBoxLayoutExample(App):
     def build(self):
-        label = Label(text='Hello from Mezidia',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
-        return label
+        layout = BoxLayout(padding=10)
+        colors = [red, green, blue, purple]
+
+        for i in range(5):
+            btn = Button(text="Button #%s" % (i + 1),
+                         background_color=random.choice(colors)
+                         )
+
+            layout.add_widget(btn)
+        return layout
 
 
-if __name__ == '__main__':
-    app = MainApp()
+if __name__ == "__main__":
+    app = HBoxLayoutExample()
     app.run()
