f1d9cd7cbbe66f891bb9ac516dc94d81ab6ccac0
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-11 20:36:09 +0300

    Update main.py
    Hello script from Kivy

--- mobileApp/main.py
+++ mobileApp/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivy.app import App
+from kivy.uix.label import Label
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainApp(App):
+    def build(self):
+        label = Label(text='Hello from Mezidia',
+                      size_hint=(.5, .5),
+                      pos_hint={'center_x': .5, 'center_y': .5})
+        return label
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = MainApp()
+    app.run()
