5d1f472ff12a501a078b60fb95b9d9318b0e4775
VsIG <valentyn2202@gmail.com>
2021-04-18 22:04:13 +0300

    ✨: add WORKING bot and dJango run

--- start.py
+++ start.py
@@ -3,22 +3,6 @@ Main script that launch the programm
 """
 import os
 
-def start_bot():
-    """
-    Script to launch the bot
-    """
-    os.system('python bot/bot.py')
-    print('Launch the bot')
-
-
-def start_server():
-    """
-    Script to launch the server
-    """
-    os.system('python server/manage.py runserver 0.0.0.0:8000')
-    print('Launch the server')
-
 
 if __name__ == '__main__':
-    start_bot()
-    start_server()
+    os.system('python bot/bot.py & python server/manage.py runserver 0.0.0.0:8000')
