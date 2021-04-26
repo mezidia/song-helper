32ef2cc01e4a6f3968387296c1d0de045ad0546b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-26 14:59:21 +0300

    #36 #35 Update main.py
    Change some names

--- mobileApp/main.py
+++ mobileApp/main.py
@@ -1,7 +1,6 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.button import MDFlatButton
 from kivy.core.window import Window
 from kivy.lang import Builder
@@ -21,10 +20,10 @@ class MainApp(MDApp):
         Constructor for class. Set all elements here
         """
         super().__init__()
-        self.label = Builder.load_string(styles.Label)
+        self.greeting_label = Builder.load_string(styles.Label)
         self.input_data = Builder.load_string(styles.TextField)
-        self.button = MDFlatButton(text='Search the user in GitHub API', pos_hint={'center_x': 0.5, 'center_y': 0.5}, on_release=self.input_btn_callback)
-        self.link = MDFlatButton(text='[b]Here[/b] will be your link', markup=True, pos_hint={'center_x': 0.5, 'center_y': 0.5}, on_release=self.link_btn_callback)
+        self.search_button = MDFlatButton(text='Search the user in GitHub API', pos_hint={'center_x': 0.5, 'center_y': 0.5}, on_release=self.input_btn_callback)
+        self.result_link = MDFlatButton(text='[b]Here[/b] will be your link', markup=True, pos_hint={'center_x': 0.5, 'center_y': 0.5}, on_release=self.link_btn_callback)
 
     def input_btn_callback(self, instance):
         """
@@ -34,15 +33,15 @@ class MainApp(MDApp):
         """
         input_text = self.input_data.text
         if not input_text:
-            self.link.text = 'Enter the nickname'
+            self.result_link.text = 'Enter the nickname'
         else:
             loop = asyncio.get_event_loop()
             link = loop.run_until_complete(make_request(input_text))
             self.input_data.text = ''
             try:
-                self.link.text = link['html_url']
+                self.result_link.text = link['html_url']
             except KeyError:
-                self.link.text = 'This user is not found'
+                self.result_link.text = 'This user is not found'
 
     def link_btn_callback(self, instance):
         """
@@ -69,19 +68,18 @@ class MainApp(MDApp):
         Override build function
         :return: box layout
         """
-        # TODO: Make label, input, button in layouts and add to screen them
         self.title = 'Song Helper'
         self.theme_cls.primary_palette='Blue'
         self.theme_cls.theme_style = 'Dark'
         screen = MDScreen()
-        box = MDBoxLayout(orientation='vertical')
-        box.add_widget(Builder.load_string(styles.Toolbar))
-        box.add_widget(self.label)
-        box.add_widget(self.input_data)
-        box.add_widget(self.button)
-        box.add_widget(self.link)
+        mainBox = MDBoxLayout(orientation='vertical')
+        mainBox.add_widget(Builder.load_string(styles.Toolbar))
+        mainBox.add_widget(self.greeting_label)
+        mainBox.add_widget(self.input_data)
+        mainBox.add_widget(self.search_button)
+        mainBox.add_widget(self.result_link)
 
-        screen.add_widget(box)
+        screen.add_widget(mainBox)
         return screen
 
 
