e457231c37f8ad075f7a76d059d5bdc29be45d8d
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-30 14:56:51 +0300

    #36 #35 Create test_main.py
    Test async function

---
+++ mobileApp/test_main.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import asyncio
+from main import make_request
+
+
+class Test(TestCase):
+    def setUp(self) -> None:
+        self.expected = {'success': 'https://github.com/mezgoodle', 'failure': 'dadas@@@#ssf'}
+        self.data = {'success': 'mezgoodle', 'failure': 'dadas@@@#ssf'}
+
+    def test_make_request_success(self):
+        loop = asyncio.get_event_loop()
+        result = loop.run_until_complete(make_request(self.data['success']))
+        self.assertEqual(result['html_url'], self.expected['success'])
+
+    def test_make_request_failure(self):
+        loop = asyncio.get_event_loop()
+        result = loop.run_until_complete(make_request(self.data['failure']))
+        self.assertFalse('html_url' in result)
