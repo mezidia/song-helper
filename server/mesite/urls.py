a3e671f8f76bd43c8b2e95fed70020e56ee2f185
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-07 22:46:02 +0300

    Create function to add the song

--- server/mesite/urls.py
+++ server/mesite/urls.py
@@ -6,5 +6,6 @@ app_name = 'mesite'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add-song', views.add_song, name='add_song'),
-    path('get-song/<str:text>', views.get_song, name='get_song')
+    path('get-song/<str:text>', views.get_song, name='get_song'),
+    path('add-song-resp/<str:song_id>', views.get_song, name='add_song_resp')
 ]
--- server/mesite/views.py
+++ server/mesite/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from datetime import date
-import requests
 import json
 from .forms import MainForm, AddForm
 
@@ -32,15 +31,9 @@ def add_song(request):
         if form.is_valid():
             song_id = form.cleaned_data['song_id']
             try:
-                r = requests.get('https://api.github.com/users/mezgoodle')
-                login = r.json()['login']
-                # TODO: go to song-helper/predict.py and get mood
-                # result = predict_mood(song_id)
-                # TODO: go to song-helper/utils.py and get song feature
-                # features = get_song_features(song_id)
-                # TODO: save new object to model
-                # Song.save(features)
-                result = f'Song with id "{song_id}" added as Happy {login}'
+                data = add_song_resp(request, song_id)
+                mood = json.loads(data.content)['mood']
+                result = f'Song with id "{song_id}" added as {mood}'
             except:
                 result = 'Cannot find your song'
     context = {
@@ -63,5 +56,21 @@ def get_song(request, text):
         'mood': text,
         'song': song_name,
         'link': link
+    }
+    return JsonResponse(data)
+
+
+def add_song_resp(request, song_id):
+    # TODO: go to song-helper/predict.py and get mood
+    # result = predict_mood(song_id)
+    # TODO: go to song-helper/utils.py and get song feature
+    # features = get_song_features(song_id)
+    # TODO: save new object to model
+    # Song.save(features)
+    song_name, result = 'Test name', 'Test mood'
+    data = {
+        'success': 'true',
+        'song': song_name,
+        'mood': result
     }
     return JsonResponse(data)
