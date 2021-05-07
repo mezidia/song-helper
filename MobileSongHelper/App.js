d1c38fa8350fad68c39f39186a2b5c50384be23a
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-07 10:05:06 +0300

    Update App.js
    Custom styles

--- MobileSongHelper/App.js
+++ MobileSongHelper/App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
