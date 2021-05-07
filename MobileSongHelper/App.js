d4bef3606eb9bc564c9d04767e827a7110c99f6a
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-07 13:28:13 +0300

    Update App.js
    Create input tag

--- MobileSongHelper/App.js
+++ MobileSongHelper/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image, ImageBackground} from 'react-native';
+import {Text, View, StyleSheet, Image, ImageBackground, TextInput, Dimensions} from 'react-native';
 import Header from "./components/Header";
 import Input from "./components/Input";
 
+const {width: WIDTH} = Dimensions.get('window')
+
 const App = () => {
   return (
-      <ImageBackground source={'https://i.pinimg.com/originals/b9/84/00/b98400eea2b884c8950c77d9072c6256.jpg'}
+      <ImageBackground source={{uri: 'https://i.pinimg.com/originals/b9/84/00/b98400eea2b884c8950c77d9072c6256.jpg'}}
                        style={styles.backgroundContainer}>
         <View style={styles.logoContainer}>
-          <Image source={'https://raw.githubusercontent.com/mezgoodle/images/master/MezidiaLogoTransparent.png'}
+          <Image source={{uri: 'https://raw.githubusercontent.com/mezgoodle/images/master/MezidiaLogoTransparent.png'}}
                  style={styles.logo}/>
-          <Text style={styles.logoText}>Song helper</Text>
+          <Text style={styles.logoText}>Another text</Text>
+        </View>
+        <View>
+          <TextInput
+              style={styles.input}
+              placeholder={'Username'}
+              placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+              underlineColorAndroid='transparent'
+          />
         </View>
       </ImageBackground>
   );
@@ -38,6 +48,16 @@ const styles = StyleSheet.create({
     marginTop: 10,
     opacity: 0.5,
     textTransform: 'uppercase'
+  },
+  input: {
+    width: WIDTH - 55,
+    height: 45,
+    borderRadius: 25,
+    fontSize: 16,
+    paddingLeft: 45,
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    color: 'rgba(255, 255, 255, 0.7)',
+    marginHorizontal: 25
   }
 })
 
