1d51eea7fe650f5ad26b9d19b796c12451820440
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-07 10:12:29 +0300

    Update App.js
    Add image

--- MobileSongHelper/App.js
+++ MobileSongHelper/App.js
@@ -1,24 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello world!</Text>
+        <StatusBar style="auto"/>
+        <Image
+            source={{uri: 'https://raw.githubusercontent.com/mezgoodle/images/master/MezidiaLogoTransparent.png'}}
+            style={styles.img}/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    text: {
-        color: 'darkslateblue',
-        fontSize: 30
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'darkslateblue',
+    fontSize: 30
+  },
+  img: {
+    width: 100,
+    height: 100
+  }
 })
