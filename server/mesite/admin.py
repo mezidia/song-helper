a6eb310a7bb6f17aa460a3589cf64f56508a581a
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-06-03 09:37:57 +0300

    Remove files

--- server/mesite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Song
-
-
-@admin.register(Song)
-class SongAdmin(admin.ModelAdmin):
-    list_display = ('name', 'artists', 'song_id', 'mood')
-    list_filter = ('mood',)
-    search_fields = ['name', 'artists']
--- server/songhelper/words_preprocessor.py
+++
@@ -1,7 +0,0 @@
-class WordPreprocessor:
-    """
-    Class for preprocessing some text for future use
-    """
-    # Basic function to clean the text
-    def clean_text(self, text):
-        return text.strip().lower()
