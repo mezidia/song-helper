a377bf012b18b6eb90ec9bd87d0deaecb36f89c1
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-06-05 14:08:15 +0300

    CU-85zt75k0k Set up authentication

--- server/main.py
+++ server/main.py
@@ -1,9 +1,19 @@
-from typing import Optional
+from typing import Annotated, Optional
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
 
 class Item(BaseModel):
@@ -12,9 +22,29 @@ class Item(BaseModel):
     is_offer: Optional[bool] = None
 
 
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded",
+        email="john@example.com",
+        full_name="John Doe",
+    )
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    return current_user
+
+
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def read_root(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
 
 
 @app.get("/items/{item_id}")
