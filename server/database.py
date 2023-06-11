007cd9c05753906483f5a5a221f97f01b6ea6a84
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-06-11 16:32:13 +0300

    Fix lint error

--- server/database.py
+++ server/database.py
@@ -5,7 +5,7 @@ fake_users_db = {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",  # noqa: E501
         "disabled": False,
     }
 }
