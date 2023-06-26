84a4092fb62744a55c1f0bf83e413fefe829f5ef
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-06-26 17:58:06 +0300

    CU-85zt90fuf Write schemas for tables in db

--- server/src/main.py
+++ server/src/main.py
@@ -1,3 +1,4 @@
+from database import create_db_and_tables
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from routers import authentication, requests, songs, users
@@ -7,6 +8,11 @@ app.include_router(users.router)
 app.include_router(authentication.router)
 app.include_router(requests.router)
 app.include_router(songs.router)
+
+
+@app.on_event("startup")
+async def startup():
+    create_db_and_tables()
 
 
 @app.get("/")
--- server/src/schemas.py
+++ server/src/schemas.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from pydantic import BaseModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class Token(BaseModel):
@@ -10,15 +13,60 @@ class TokenData(BaseModel):
     username: str | None = None
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+class RequestBase(SQLModel):
+    text: str
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
 
 
-class UserInDB(User):
-    hashed_password: str
+class Request(RequestBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user: Optional["User"] = Relationship(back_populates="requests")
+
+
+class RequestCreate(RequestBase):
+    pass
+
+
+class RequestRead(RequestBase):
+    id: int
+
+
+class RequestUpdate(SQLModel):
+    text: Optional[str] = None
+    user_id: Optional[int] = None
+
+
+class UserBase(SQLModel):
+    name: str = Field(index=True)
+    email: str
+
+
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    password: str = Field()
+    requests: Optional[list[Request]] = Relationship(back_populates="user")
+
+
+class UserRead(UserBase):
+    id: int
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(SQLModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+
+
+class RequestReadWithUser(RequestRead):
+    user: Optional[UserRead] = None
+
+
+class UserReadWithRequests(UserRead):
+    requests: Optional[list[RequestRead]] = None
 
 
 class Prompt(BaseModel):
