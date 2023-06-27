6657c15185a67f3cf0c27e69ad774442ecc04737
MaksGovor <maksgovruha@gmail.com>
2023-06-27 16:05:19 +0300

    CU-85zt75jhy Fix: try vercel deploy

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            By{' '}
+            Byy{' '}
             <Image
               src='/vercel.svg'
               alt='Vercel Logo'
